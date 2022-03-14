fe7bb01511afd5d7883828c3dca5dcd08a459add
stefankeler96 <stefan.keler@intens.rs>
2022-03-14 17:49:41 +0100

    intens praksa 2022

--- src/main/java/intens/api/praksa2022/controller/DemoController.java
+++ src/main/java/intens/api/praksa2022/controller/DemoController.java
@@ -5,10 +5,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class demoController {
+public class DemoController {
     @RequestMapping("/")
     public ResponseEntity<String> getRootDemo() {
 
-        return ResponseEntity.ok().body("Intens Praksa!");
+        return ResponseEntity.ok().body("Intens Praksa 2022!");
     }
 }
